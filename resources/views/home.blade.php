8f1d7e3bbf0a921b100b6c270e0d41906b6c0434
GhibranM <gibranm214@gmail.com>
2022-03-07 09:35:27 -0500

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>WELCOME TO MY BIODATAE</title> 
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <div class="hero">
+
+        <div class="logo">
+            <img src="{{ asset('img/logo2.png') }}">
+        </div>
+
+        <div class="sidebar">
+            <div class="text-box">
+                <h1>MOON PROJECT</h1>
+                <h4>PREMIUM QUALITY</h4>
+                <div class="line"></div>
+            </div>
+        </div>
+
+        <div class="content">
+        <h1>GHIBRAN MUHAMMAD</h1>
+        <p>TTL        : Blitar, 30 November 2000 <br>
+            Alamar    : Desa Kedungdowo kec Kaliwungu Kudus Jateng<br>
+            Agama     : Islam<br>
+            Kelamin   : Laki - Laki<br>
+            Pekerjaan : Pelajar/Mahasiswa<br>
+        </p>
+        <div class="btn" id="playBtn">
+            <img src="{{ asset('img/play.png') }}" id="icon">Watch Video
+        </div>
+        </div>
+
+        <div class="Pro">
+            <img src="{{ asset('img/gm.jpg') }}">
+        </div>
+
+    </div>
+</body>
+</html>
